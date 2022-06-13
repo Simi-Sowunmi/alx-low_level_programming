4ed978fa4f1f6d65b439ee0f267b12a7943429d8
Similoluwa Sowunmi <93050186+Simi-Sowunmi@users.noreply.github.com>
2022-06-14 00:26:31 +0100

    a function that convert a string to an integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,13 +10,13 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int index = 0;
+int index = 0;
 
-	for (index = 0; src[index] != '\0'; index++)
-	{
-		dest[index] = src[index];
-	}
-	dest[index] = '\0';
+for (index = 0; src[index] != '\0'; index++)
+{
+dest[index] = src[index];
+}
+dest[index] = '\0';
 
-	return (dest);
+return (dest);
 }
