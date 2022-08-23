049580b6fc383c5581aba3c9013d25400536a827
Simi-Sowunmi <similoluwasowunmi@gmail.com>
2022-08-23 08:14:05 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,6 +1,5 @@
 #include "main.h"
 
-<<<<<<< HEAD
 /**
  * _strcat - concatenates @src to @dest
  * @src: the source string to append to @dest
@@ -19,42 +18,3 @@ char *_strcat(char *dest, char *src)
 		dest[dest_len++] = src[index];
 	return (dest);
 }
-=======
-
-
-/**
-
- * _strcat - concatenates @src to @dest
-
- * @src: the source string to append to @dest
-
- * @dest: the destiation string to be concatenated upon
-
- * Return:pointer to the resulting string
-
- */
-
-
-
-char *_strcat(char *dest, char *src)
-
-{
-
-	int index = 0;
-
-	int dest_len = 0;
-
-
-
-	while (dest[index++])
-
-		dest_len++;
-
-	for (index = 0; src[index]; index++)
-
-		dest[dest_len++] = src[index];
-
-	return (dest);
-
-}
->>>>>>> cb6731fad2a9e17896f65ae12bb336e5a25f1b62
