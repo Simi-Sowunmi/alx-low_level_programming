3c184c8414b9bf13522b0e4f3163aa44aa4c1048
Simi-Sowunmi <similoluwasowunmi@gmail.com>
2022-08-23 07:01:12 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -4,36 +4,54 @@
 
 /**
 
- * _strncat - Concatenates two strings using at most
+ *_strncat - concatenates two strings
 
- * @dest: The string to be appended upon.
+ *@dest: A pointer to a character that will be changed
 
- * @src: source.
+ *@src: A pointer to a character that will be changed
 
- * @n: print amount of byte used from src
+ *@n: value
 
- * Return: The pointer to destination
+ *Return: dest
 
  */
-
-
 
 char *_strncat(char *dest, char *src, int n)
 
 {
 
-	int index = 0, dest_len = 0;
+int i, j;
 
 
 
-	while (dest[index++])
+i = 0;
 
-		dest_len++;
 
-	for (index = 0; src[index] && index < n; index++)
 
-		dest[dest_len++] = src[index];
+while (dest[i] != '\0')
 
-	return (dest);
+{
+
+i++;
+
+}
+
+j = 0;
+
+while (j < n && src[j] != '\0')
+
+{
+
+dest[i] = src[j];
+
+j++;
+
+i++;
+
+}
+
+dest[i] = '\0';
+
+return (dest);
 
 }
